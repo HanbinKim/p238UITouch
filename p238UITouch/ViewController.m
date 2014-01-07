9e353825f86af7ce6999b9bd80bfdcc0d148d226
HanbinKim <hottuc@naver.com>
2014-01-07 17:39:10 +0900

    p238 UITouchApple
    UITouch를 이용한 이미지 이동 실습입니다.

--- p238UITouch/ViewController.m
+++ p238UITouch/ViewController.m
@@ -9,10 +9,47 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    BOOL holdingImage;
+}
+@property (weak, nonatomic) IBOutlet UIImageView *appleImage;
 
 @end
 
 @implementation ViewController
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch locationInView:self.view];
+    
+    if(CGRectContainsPoint(self.appleImage.frame, point))
+    {
+        holdingImage = YES;
+        self.appleImage.transform = CGAffineTransformMakeScale(1.2, 1.2);
+    }
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    NSLog(@"move count:%lu",(unsigned long)[[event allTouches] count]);
+    
+    if(holdingImage)
+    {
+        UITouch *touch = [touches anyObject];
+        CGPoint point = [touch locationInView:self.view];
+        self.appleImage.center = point;
+    }
+}
+
+- (void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    if(holdingImage)
+    {
+        self.appleImage.transform = CGAffineTransformIdentity;
+        holdingImage = NO;
+    }
+}
 
 - (void)viewDidLoad
 {
